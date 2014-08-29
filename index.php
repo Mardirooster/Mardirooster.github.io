74f98a538555cbd4006a9e93228596a95250577e
Mardirooster <samuelmardirosian@gmail.com>
2014-08-29 16:38:30 +0100

    Added placeholder

--- index.php
+++ index.php
@@ -142,6 +142,8 @@
 <p><a href="register.php" style="color: #000; text-decoration: none;">registration</a></p>
 <p><a href="upload.php" style="color: #000; text-decoration: none;">upload</a></p>
 <p><a href="http://127.0.0.1:8008/" style="color: #000; text-decoration: none;">khan academy</a></p>
+<p><a href="//placeholder" style="color: #000; text-decoration: none;">khan academy</a></p>
+
 </div>
 </div>
 </div>
